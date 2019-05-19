bb44877c3f3688c7f98bc9cfdc6c23fbbede3e32
Giuseppe <gcrosti@gmail.com>
2019-05-19 17:50:07 +0000

    all tests passed

--- social_network/posts.py
+++ social_network/posts.py
@@ -4,33 +4,53 @@ from datetime import datetime
 # create these classes as it corresponds:
 # (your tests will fail if you don't comment out these classes)
 
-# class Post(object):
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def set_user(self, user):
-#         pass
+class Post(object):
+    def __init__(self, text, timestamp=None,user=None):
+        self.text = text
+        self.timestamp = timestamp
+        self.user = user
+
+    def set_user(self, user):
+        self.user = user
+        
+    def __str__(self):
+        username = "@" + self.user.first_name + " " + self.user.last_name
+        time = ''
+        if self.timestamp:
+            time = self.timestamp.strftime('%A') + ', ' + self.timestamp.strftime('%b') + ' ' + self.timestamp.strftime('%d') + ', ' + self.timestamp.strftime('%Y')
+        return '{}: "{}"\n\t{}'.format(username,self.text,time)
+    
+    
+class TextPost(Post):  # Inherit properly
+    pass
 
 
-# class TextPost(...):  # Inherit properly
-#     def __init__(self, text, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class PicturePost(Post):  # Inherit properly
+    def __init__(self,text,timestamp,image_url):
+        self.text = text
+        self.timestamp = timestamp
+        self.image_url = image_url
+
+    def __str__(self):
+        textpost = super().__str__()
+        ind = textpost.find('\n')
+        picpost = textpost[:ind] +'\n\t'+ self.image_url + textpost[ind:]
+        return picpost
 
 
-# class PicturePost(...):  # Inherit properly
-#     def __init__(self, text, image_url, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
-
-
-# class CheckInPost(...):  # Inherit properly
-#     def __init__(self, text, latitude, longitude, timestamp=None):
-#         pass
-#
-#     def __str__(self):
-#         pass
+class CheckInPost(Post):  # Inherit properly
+    def __init__(self,latitude,longitude,text,timestamp):
+        self.text = text
+        self.timestamp = timestamp
+        self.latitude = latitude
+        self.longitude = longitude
+        
+    def __str__(self):
+        textpost = super().__str__()
+        firstname = textpost[:textpost.find(' ')]
+        start = textpost.find(' ',len(firstname)+1)
+        ind = textpost.find('\n')
+        position = str(self.latitude) + ', ' + str(self.longitude)
+        checkpost = firstname + ' Checked In:' + textpost[start:ind] + '\n\t' + position + textpost[ind:]
+        print(checkpost)
+        return checkpost
